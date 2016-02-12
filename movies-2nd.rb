191749bb0f67fbca6f4258f9556a4fd54f2ad770
Diethart <diethart19@gmail.com>
2016-02-12 20:27:52 +0300

    Update movies-2nd.rb

--- movies-2nd.rb
+++ movies-2nd.rb
@@ -5,13 +5,12 @@ end
 filmstop = File.open(ARGV[0]) { |file| file.readlines }
 filmstop.each do |film|
   filmstopseparated = film.split('|')
-  
   if filmstopseparated[1].include?("Time")
     rating =  filmstopseparated[7].to_f - filmstopseparated[7].to_i 
-	print filmstopseparated[1], " "
-	for i in 1..(rating*10).round
-	  print "*"
+    print filmstopseparated[1], " "
+    for i in 1..(rating*10).round
+      print "*"
     end
-	print "\n"
+    print "\n"
   end
 end
