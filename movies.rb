50cc721fd0ab3574ab24bdedd523593159cde913
Diethart <diethart19@gmail.com>
2016-02-12 12:07:58 +0300

    Create first project
    Firs project

---
+++ movies.rb
@@ -0,0 +1,10 @@
+matrixvalue = "matrix"
+titanicvalue = "titanic"
+
+if ARGV[0] == matrixvalue
+	puts "Matrix is a good film"
+elsif ARGV[0] == titanicvalue
+	puts "Titanic is also a good film"
+else
+	puts "I haven't seen this movie yet"
+end
