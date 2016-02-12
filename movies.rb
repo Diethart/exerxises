1c54e9cce26fe1bff275f25562cd92a4680ba220
Diethart <diethart19@gmail.com>
2016-02-12 15:21:34 +0300

    Небольшая модификация
    Добавил бонусную часть

--- movies.rb
+++ movies.rb
@@ -1,10 +1,24 @@
 matrixvalue = "matrix"
 titanicvalue = "titanic"
 
-if ARGV[0] == matrixvalue
+arg = ARGV.join(" ")
+
+if arg == matrixvalue
 	puts "Matrix is a good film"
-elsif ARGV[0] == titanicvalue
+elsif arg == titanicvalue
 	puts "Titanic is also a good film"
 else
 	puts "I haven't seen this movie yet"
 end
+
+goodfilms = ["matrix", "citizen kane", "mad max"]
+badfilms = ["green elephant", "elki", "best movie"]
+
+if goodfilms.include?(arg)
+	puts "This film in the lists of good films"
+elsif badfilms.include?(arg)
+	puts "This film in the lists of bad films"
+else
+	puts "This film is not in the list"
+end
+
