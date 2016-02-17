64c3ecc31bd557278408a43fc5d84034d6fb9712
Diethart <diethart19@gmail.com>
2016-02-17 14:50:14 +0300

    Добавил тесты

--- movies/main.rb
+++ movies/main.rb
@@ -1,5 +1,15 @@
 require_relative 'movie'
 require_relative 'movies_list'
+require 'pp'
 
 movies = MoviesList.new(ARGV[0],"|")
-movies.select_genre("COMEDY").each { |film| puts film.to_s }
+
+#puts movies.longest(5)
+#puts movies.select_genre("COMEDY")
+#puts movies.reject_genre("COMEDY")
+#puts movies.select_country("USA")
+#puts movies.reject_country("USA")
+#puts movies.director_stat
+#puts movies.director_names
+#pp movies.actors_stat
+pp movies.month_stat
